5e733153f51f37b75f009bb7156357e83d7667f4
Fhmonly <rynare.of.legend@gmail.com>
2026-02-10 23:11:37 +0700

    implementasi api untuk news detail

--- src/services/news.service.ts
+++ src/services/news.service.ts
@@ -5,11 +5,25 @@ export interface Kategori {
     id: number;
     nama: string;
 }
+
+export interface KepalaProgram {
+    id: number;
+    nama: string;
+    gelar_depan: string;
+    gelar_belakang: string;
+}
+
 export interface Prodi {
     id: number;
     nama: string;
-    gelar: 'D3' | 'S1' | 'S2' | 'S3'; // bisa diperluas
+    gelar: 'D3' | 'S1' | 'S2' | 'S3';
+    akreditasi: string;
+    kepala_program: KepalaProgram;
+    deskripsi: string;
+    aktif: boolean;
+    visi_misi: string;
 }
+
 
 export interface Artikel {
     id: number;
@@ -20,6 +34,19 @@ export interface Artikel {
     konten: string;
     prodi: Prodi;
     gambar_unggulan: string;
+    hightlight: boolean;
+    tanggal: string; // ISO string
+}
+
+export interface ArtikelDetail {
+    id: number;
+    judul: string;
+    slug: string;
+    konten: string;
+    kutipan: string;
+    kategori: Kategori;
+    gambar_unggulan: string;
+    prodi: Prodi;
     hightlight: boolean;
     tanggal: string; // ISO string
 }
@@ -37,6 +64,14 @@ export const newsService = {
             const param = new URLSearchParams();
             param.set('page', page.toString());
             return await http.get<NewsListResponse>(`/berita?${param.toString()}`);
+        } catch (error) {
+            return null
+        }
+    },
+    getNewsDetail: async (id?: string) => {
+        try {
+            if (!id) return null
+            return await http.get<ArtikelDetail>(`/berita/${id}`);
         } catch (error) {
             return null
         }
